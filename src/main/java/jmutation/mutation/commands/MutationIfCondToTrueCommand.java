0f356ea82265eee899041805124f21596679d5be
bchenghi <bohchenghin@gmail.com>
2022-07-23 16:42:18 +0800

    Add if stmt cond to true mutation

---
+++ src/main/java/jmutation/mutation/commands/MutationIfCondToTrueCommand.java
@@ -0,0 +1,22 @@
+package jmutation.mutation.commands;
+
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.Expression;
+import org.eclipse.jdt.core.dom.IfStatement;
+
+public class MutationIfCondToTrueCommand extends MutationCommand {
+
+    public MutationIfCondToTrueCommand(ASTNode node) {
+        super(node);
+    }
+
+    @Override
+    public ASTNode executeMutation() {
+        IfStatement ifStatement = (IfStatement) node;
+        AST ast = ifStatement.getAST();
+        Expression newExpression = ast.newBooleanLiteral(true);
+        ifStatement.setExpression(newExpression);
+        return ifStatement;
+    }
+}
--- src/main/java/jmutation/mutation/parser/MutationParser.java
+++ src/main/java/jmutation/mutation/parser/MutationParser.java
@@ -4,12 +4,14 @@ import jmutation.mutation.MathOperator;
 import jmutation.mutation.commands.MutationBlockRemovalCommand;
 import jmutation.mutation.commands.MutationCommand;
 import jmutation.mutation.commands.MutationForLoopToIfCommand;
+import jmutation.mutation.commands.MutationIfCondToTrueCommand;
 import jmutation.mutation.commands.MutationMathOperatorCommand;
 import jmutation.mutation.commands.MutationWhileLoopToIfCommand;
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.Block;
 import org.eclipse.jdt.core.dom.ForStatement;
+import org.eclipse.jdt.core.dom.IfStatement;
 import org.eclipse.jdt.core.dom.InfixExpression;
 import org.eclipse.jdt.core.dom.InfixExpression.Operator;
 import org.eclipse.jdt.core.dom.WhileStatement;
@@ -28,6 +30,11 @@ public class MutationParser extends ASTVisitor {
     public MutationCommand parse(ASTNode node) {
         node.accept(this);
         return command;
+    }
+
+    @Override
+    public void preVisit(ASTNode node) {
+        command = null;
     }
 
     @Override
@@ -53,6 +60,12 @@ public class MutationParser extends ASTVisitor {
     @Override
     public boolean visit(Block node) {
         command = new MutationBlockRemovalCommand(node);
+        return false;
+    }
+
+    @Override
+    public boolean visit(IfStatement node) {
+        command = new MutationIfCondToTrueCommand(node);
         return false;
     }
 }
