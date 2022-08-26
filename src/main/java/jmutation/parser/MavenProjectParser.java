75df00e6817e776fae0c0f92f783e3f5255ed840
bchenghi <bohchenghin@gmail.com>
2022-08-26 16:26:53 +0800

    Fix test case

--- src/main/java/jmutation/parser/MavenProjectParser.java
+++ src/main/java/jmutation/parser/MavenProjectParser.java
@@ -7,12 +7,14 @@ import java.util.List;
 import java.util.function.Predicate;
 
 import jmutation.constants.MavenConstants;
+import org.apache.maven.model.Build;
 import org.apache.maven.model.Model;
 import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
 import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 
 public class MavenProjectParser extends ProjectParser {
     Model model;
+    Build build;
     String rootPath;
 
     public MavenProjectParser(File root) {
@@ -22,6 +24,7 @@ public class MavenProjectParser extends ProjectParser {
         MavenXpp3Reader reader = new MavenXpp3Reader();
         try {
             model = reader.read(new FileReader(pomFile));
+            build = model.getBuild();
         } catch (XmlPullParserException | IOException e) {
             System.out.println("pom not found");
         }
@@ -32,37 +35,53 @@ public class MavenProjectParser extends ProjectParser {
     }
 
     public String getSrcFolderPath() {
-        String srcFolderPath = model.getBuild().getSourceDirectory();
-        if (srcFolderPath == null) {
-            srcFolderPath = rootPath + File.separator + MavenConstants.SRC_FOLDER;
+        String srcFolderPath = rootPath + File.separator + MavenConstants.SRC_FOLDER;
+        if (build == null) {
+            return srcFolderPath;
         }
-        return rootPath + srcFolderPath;
+        String pomPath = build.getSourceDirectory();
+        if (pomPath == null) {
+            return srcFolderPath;
+        }
+        return rootPath + pomPath;
     }
 
     public String getTestFolderPath() {
-        String testFolderPath = model.getBuild().getTestSourceDirectory();
-        if (testFolderPath == null) {
-            testFolderPath = File.separator + MavenConstants.TEST_FOLDER;
+        String testFolderPath = rootPath + File.separator + MavenConstants.TEST_FOLDER;
+        if (build == null) {
+            return testFolderPath;
         }
-        return rootPath + testFolderPath;
+        String pomPath = build.getTestSourceDirectory();
+        if (pomPath == null) {
+            return testFolderPath;
+        }
+        return rootPath + pomPath;
     }
 
     public String getCompiledSrcFolderPath() {
-        String outputPath = model.getBuild().getOutputDirectory();
-        if (outputPath == null) {
-            outputPath = rootPath + File.separator + MavenConstants.COMPILATION_FOLDER + File.separator +
-                    MavenConstants.COMPILED_CLASS_FOLDER;
+        String outputPath = MavenConstants.COMPILATION_FOLDER + File.separator +
+                MavenConstants.COMPILED_CLASS_FOLDER;
+        if (build == null) {
+            return outputPath;
         }
-        return rootPath + outputPath;
+        String pomPath = build.getOutputDirectory();
+        if (pomPath == null) {
+            return outputPath;
+        }
+        return rootPath + pomPath;
     }
 
     public String getCompiledTestFolderPath() {
-        String testOutputPath = model.getBuild().getTestOutputDirectory();
-        if (testOutputPath == null) {
-            testOutputPath =  rootPath + File.separator + MavenConstants.COMPILATION_FOLDER + File.separator +
-                    MavenConstants.TEST_CLASS_FOLDER;
+        String testOutputPath = MavenConstants.COMPILATION_FOLDER + File.separator +
+                MavenConstants.TEST_CLASS_FOLDER;
+        if (build == null) {
+            return testOutputPath;
         }
-        return rootPath + testOutputPath;
+        String pomPath = build.getTestOutputDirectory();
+        if (pomPath == null) {
+            return testOutputPath;
+        }
+        return rootPath + pomPath;
     }
 
     private File getPomFile() {
