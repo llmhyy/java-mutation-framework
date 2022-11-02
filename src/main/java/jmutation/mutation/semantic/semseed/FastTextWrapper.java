78ab8cecbe0853bcd3542e5cd43ca364a1730146
bchenghi <bohchenghin@gmail.com>
2022-11-02 22:52:47 +0800

    Add obtaining replacement token feature

--- src/main/java/jmutation/mutation/semantic/semseed/FastTextWrapper.java
+++ src/main/java/jmutation/mutation/semantic/semseed/FastTextWrapper.java
@@ -1,13 +1,18 @@
 package jmutation.mutation.semantic.semseed;
 
 import com.github.jfasttext.JFastText;
+import jmutation.mutation.semantic.semseed.model.Pattern;
+import jmutation.mutation.semantic.semseed.model.TokenSequence;
+import org.gradle.internal.Pair;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class FastTextWrapper {
 
+    private final float threshold;
     JFastText jft;
-    private float threshold;
 
 
     public FastTextWrapper(String pathToModel) {
@@ -22,7 +27,7 @@ public class FastTextWrapper {
         this.threshold = threshold;
     }
 
-    private static double cosineSimilarity(List<Float> vectorA, List<Float> vectorB) {
+    private static double cosineDistance(List<Float> vectorA, List<Float> vectorB) {
         double dotProduct = 0.0;
         double normA = 0.0;
         double normB = 0.0;
@@ -43,8 +48,70 @@ public class FastTextWrapper {
             String fixToken = fixConcreteTokens.get(i);
             List<Float> targetTokenEmb = jft.getVector(targetToken);
             List<Float> fixTokenEmb = jft.getVector(fixToken);
-            sumOfSimilarities += cosineSimilarity(targetTokenEmb, fixTokenEmb);
+            sumOfSimilarities += cosineDistance(targetTokenEmb, fixTokenEmb);
         }
         return sumOfSimilarities / lengthOfSeq > threshold;
     }
+
+    public List<String> getReplacementTokens(String buggyConcreteToken, Pattern pattern, TokenSequence targetTokenSeq,
+                                             List<String> candidates) {
+        List<String> result = new ArrayList<>();
+        List<String> targetConcrete = targetTokenSeq.getConcreteTokens();
+        List<String> correctConcrete = pattern.getFixConcrete();
+        List<List<Float>> analogousTokenEmbeddings = new ArrayList<>();
+        List<Float> buggyConcreteEmb = jft.getVector(buggyConcreteToken);
+        for (int i = 0; i < targetConcrete.size(); i++) {
+            List<Float> targetTokenEmb = jft.getVector(targetConcrete.get(i));
+            List<Float> correctTokenEmb = jft.getVector(correctConcrete.get(i));
+            List<Float> analogousEmbedding = addOrSubtractVec(buggyConcreteEmb, targetTokenEmb, true);
+            analogousEmbedding = addOrSubtractVec(analogousEmbedding, correctTokenEmb, false);
+            analogousTokenEmbeddings.add(analogousEmbedding);
+        }
+        List<Float> sumOfAllVectors = sumOfAllVec(analogousTokenEmbeddings);
+        int numOfAnalogousEmb = analogousTokenEmbeddings.size();
+        List<Float> avgOfAllAnalogousEmb = new ArrayList<>();
+        for (Float vectorVal : sumOfAllVectors) {
+            avgOfAllAnalogousEmb.add(vectorVal / numOfAnalogousEmb);
+        }
+        // Calculate similarity score for each candidate.
+        // Get candidates.
+        // Get similarity list, each index contains a score for some candidate.
+        // Choose the one with highest sim
+        List<Pair<Integer, Double>> similaritiesWithIdx = new ArrayList<>();
+        List<Float> candidateVector;
+        for (int i = 0; i < candidates.size(); i++) {
+            String candidate = candidates.get(i);
+            candidateVector = jft.getVector(candidate);
+            similaritiesWithIdx.add(Pair.of(i, 1 - cosineDistance(avgOfAllAnalogousEmb, candidateVector)));
+        }
+        Collections.sort(similaritiesWithIdx, (a, b) -> {
+            if (a.right.equals(b.right)) return 0;
+            if (a.right < b.right) return 1;
+            return -1;
+        });
+        for (Pair<Integer, Double> idxToSimPair : similaritiesWithIdx) {
+            result.add(candidates.get(idxToSimPair.left));
+        }
+        return result;
+    }
+
+    private List<Float> addOrSubtractVec(List<Float> ls0, List<Float> ls1, boolean isAddition) {
+        assert (ls0.size() == ls1.size());
+        List<Float> result = new ArrayList<>();
+        for (int i = 0; i < ls0.size(); i++) {
+            float firstFloat = ls0.get(i);
+            float secondFloat = ls1.get(i);
+            result.add(isAddition ? firstFloat + secondFloat : firstFloat - secondFloat);
+        }
+        return result;
+    }
+
+    private List<Float> sumOfAllVec(List<List<Float>> vectors) {
+        List<Float> result = vectors.get(0);
+        for (int i = 1; i < vectors.size(); i++) {
+            result = addOrSubtractVec(result, vectors.get(i), true);
+        }
+        return result;
+    }
+
 }
