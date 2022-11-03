4a128f860c18a40b5832612ac87f2116ce4e378f
bchenghi <bohchenghin@gmail.com>
2022-11-03 19:32:09 +0800

    Update test helper with using ASTRewrite

--- src/test/java/jmutation/mutation/MutationTestHelper.java
+++ src/test/java/jmutation/mutation/MutationTestHelper.java
@@ -1,15 +1,22 @@
 package jmutation.mutation;
 
+import jmutation.mutation.commands.MutationCommand;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTParser;
 import org.eclipse.jdt.core.dom.AbstractTypeDeclaration;
 import org.eclipse.jdt.core.dom.BodyDeclaration;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
+import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
+import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.Document;
 import org.eclipse.jface.text.IDocument;
+import org.eclipse.text.edits.MalformedTreeException;
+import org.eclipse.text.edits.TextEdit;
 
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MutationTestHelper {
     CompilationUnit cu;
@@ -36,5 +43,24 @@ public class MutationTestHelper {
 
     public CompilationUnit getCompilationUnit() {
         return cu;
+    }
+
+    public IDocument updateDocument(ASTRewrite rewriter, String fileContent) {
+        IDocument document = new Document(fileContent);
+        TextEdit edits = rewriter.rewriteAST(document, null);
+        try {
+            edits.apply(document);
+        } catch (MalformedTreeException | BadLocationException e) {
+            e.printStackTrace();
+        }
+        return document;
+    }
+
+    public void checkMutation(MutationCommand command, String originalDocument, String expectedDocument) {
+        IDocument mutatedDocument = updateDocument(command.getRewriter(), originalDocument);
+        parseDocStr(mutatedDocument.get());
+        CompilationUnit mutatedCU = cu;
+        parseDocStr(expectedDocument);
+        assertEquals(cu.toString(), mutatedCU.toString());
     }
 }
