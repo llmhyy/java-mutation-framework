02582ca40898b610ff0ce8aa0b88990a9098ed8d
bchenghi <bohchenghin@gmail.com>
2022-11-07 12:22:05 +0800

    Create FastTextWrapperStub for tests

---
+++ src/test/java/jmutation/mutation/semantic/FastTextWrapperStub.java
@@ -0,0 +1,31 @@
+package jmutation.mutation.semantic;
+
+import jmutation.mutation.semantic.semseed.FastTextWrapper;
+import jmutation.mutation.semantic.semseed.model.Pattern;
+import jmutation.mutation.semantic.semseed.model.TokenSequence;
+
+import java.util.List;
+
+public class FastTextWrapperStub extends FastTextWrapper {
+    private List<String> replacementTokens;
+    private boolean isMatching;
+
+    @Override
+    public List<String> getReplacementTokens(String buggyConcreteToken, Pattern pattern, TokenSequence targetTokenSeq,
+                                             List<String> candidates) {
+        return replacementTokens;
+    }
+
+    @Override
+    public boolean isSemanticallyMatching(List<String> targetConcreteTokens, List<String> fixConcreteTokens) {
+        return isMatching;
+    }
+
+    public void setReplacementTokens(List<String> replacementTokens) {
+        this.replacementTokens = replacementTokens;
+    }
+
+    public void setMatching(boolean matching) {
+        isMatching = matching;
+    }
+}
