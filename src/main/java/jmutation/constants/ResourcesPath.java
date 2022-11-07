866e0cd2b34a73acd008593d321b6058a490cf1f
bchenghi <bohchenghin@gmail.com>
2022-11-07 11:17:50 +0800

    Update name of model

--- src/main/java/jmutation/constants/ResourcesPath.java
+++ src/main/java/jmutation/constants/ResourcesPath.java
@@ -7,6 +7,6 @@ public class ResourcesPath {
             System.getenv("USERPROFILE"), "lib", "resources", "java-mutation-framework");
     final public static String DEFAULT_DROP_INS_DIR = "lib";
     final public static String DEFAULT_SEMSEED_DIR = "semantic";
-    final public static String DEFAULT_SEMSEED_MODEL = "model.bin";
+    final public static String DEFAULT_SEMSEED_MODEL = "token-embeddings-FT.bin";
     final public static String DEFAULT_SEMSEED_PATTERNS = "bug-fix-patterns.json";
 }
