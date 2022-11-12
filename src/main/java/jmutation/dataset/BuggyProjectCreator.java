875c4915667626a5292dc45bd5b2ce0bce2fb548
bchenghi <bohchenghin@gmail.com>
2022-11-13 00:06:53 +0800

    Use BuggyProject class for dataset creation

--- src/main/java/jmutation/dataset/BuggyProjectCreator.java
+++ src/main/java/jmutation/dataset/BuggyProjectCreator.java
@@ -2,9 +2,8 @@ package jmutation.dataset;
 
 import jmutation.MutationFramework;
 import jmutation.model.MutationResult;
-import jmutation.model.TestCase;
-import jmutation.mutation.MutationCommand;
 import jmutation.mutation.heuristic.HeuristicMutator;
+import org.json.JSONObject;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -14,18 +13,17 @@ import static jmutation.dataset.DatasetCreator.bugId;
 
 public class BuggyProjectCreator implements Runnable {
     private static final Object lock = new Object();
+    private static final Object storageFileLock = new Object();
     private final String repositoryPath;
     private final String projectPath;
-    private final String projectName;
-    private final TestCase testCase;
-    private final MutationCommand command;
+    private final String storageFilePath;
+    private final BuggyProject buggyProject;
 
-    public BuggyProjectCreator(String repositoryPath, String projectPath, String projectName, TestCase testCase, MutationCommand command) {
+    public BuggyProjectCreator(String repositoryPath, String projectPath, BuggyProject buggyProject, String storageFilePath) {
         this.repositoryPath = repositoryPath;
         this.projectPath = projectPath;
-        this.projectName = projectName;
-        this.testCase = testCase;
-        this.command = command;
+        this.buggyProject = buggyProject;
+        this.storageFilePath = storageFilePath;
     }
 
     private static void createFile(String contents, String pathToFile, String fileName) {
@@ -42,24 +40,25 @@ public class BuggyProjectCreator implements Runnable {
         MutationFramework mutationFramework = new MutationFramework();
         mutationFramework.setProjectPath(projectPath);
         mutationFramework.setMutator(new HeuristicMutator());
-        StringBuilder mutatedProjPath = new StringBuilder(repositoryPath + File.separator + projectName + File.separator);
+        StringBuilder mutatedProjPath = new StringBuilder(repositoryPath + File.separator + buggyProject.getProjectName() + File.separator);
         int mutatedProjPathLen = mutatedProjPath.length();
         int currBugId = increaseAndGetBugId();
         mutatedProjPath.append(currBugId);
         mutatedProjPath.append(File.separator);
         int mutatedBugPathLen = mutatedProjPath.length();
-        mutatedProjPath.append("bug");
+        mutatedProjPath.append(DatasetCreator.BUGGY_PROJECT_DIR);
         mutationFramework.setMutatedProjectPath(mutatedProjPath.toString());
         mutatedProjPath.delete(mutatedBugPathLen, mutatedBugPathLen + 3);
-        mutationFramework.setTestCase(testCase);
+        mutationFramework.setTestCase(buggyProject.getTestCase());
         try {
-            MutationResult result = mutationFramework.mutate(command);
+            MutationResult result = mutationFramework.mutate(buggyProject.getCommand());
             if (result.getMutatedPrecheckExecutionResult().testCasePassed()) {
                 mutatedProjPath.delete(mutatedProjPathLen, mutatedProjPath.length());
                 return;
             }
-            createFile(testCase.toString(), mutatedProjPath.toString(), "testcase.txt");
-            createFile(command.toString(), mutatedProjPath.toString(), "rootcause.txt");
+            createFile(buggyProject.getTestCase().toString(), mutatedProjPath.toString(), "testcase.txt");
+            createFile(buggyProject.getCommand().toString(), mutatedProjPath.toString(), "rootcause.txt");
+            writeToStorageFile();
         } catch (RuntimeException e) {
             System.out.println(e);
         }
@@ -70,6 +69,19 @@ public class BuggyProjectCreator implements Runnable {
             System.out.println(Thread.currentThread().getName() + " : " + bugId);
             bugId++;
             return bugId;
+        }
+    }
+
+    private void writeToStorageFile() {
+        synchronized (storageFileLock) {
+            System.out.println(Thread.currentThread().getName() + " : writing to json file");
+            JSONObject storedJSON = DatasetCreator.getStoredProjects(storageFilePath);
+            storedJSON.put(buggyProject.key(), buggyProject.createJSONObject());
+            try (FileWriter writer = new FileWriter(storageFilePath)) {
+                writer.write(storedJSON.toString());
+            } catch (IOException e) {
+
+            }
         }
     }
 }
--- src/main/java/jmutation/dataset/DatasetCreator.java
+++ src/main/java/jmutation/dataset/DatasetCreator.java
@@ -4,7 +4,10 @@ import jmutation.MutationFramework;
 import jmutation.model.TestCase;
 import jmutation.mutation.MutationCommand;
 import jmutation.mutation.heuristic.HeuristicMutator;
+import jmutation.utils.JSONWrapper;
 import org.apache.commons.io.FileUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.File;
 import java.io.IOException;
@@ -13,6 +16,9 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class DatasetCreator {
+    public static final String BUGGY_PROJECT_DIR = "bug";
+    public static final String WORKING_PROJECT_DIR = "fix";
+    private static final String CREATED_BUGGY_PROJECT_FILE = "createdBugs.json";
     public static int bugId = 0;
 
     public static void main(String[] args) {
@@ -27,7 +33,7 @@ public class DatasetCreator {
         // Copy from microbat
         // Create a new thread for each test case + command pair
         String projectName = projectPath.substring(projectPath.lastIndexOf(File.separator) + 1);
-        File fixPath = new File(String.join(File.separator, repositoryPath, projectName, "fix"));
+        File fixPath = new File(String.join(File.separator, repositoryPath, projectName, WORKING_PROJECT_DIR));
         try {
             FileUtils.deleteDirectory(fixPath);
             FileUtils.copyDirectory(new File(projectPath), fixPath);
@@ -35,12 +41,14 @@ public class DatasetCreator {
             e.printStackTrace();
             throw new RuntimeException("Failed to clone project to " + fixPath.getAbsolutePath());
         }
-        int numOfCores = Runtime.getRuntime().availableProcessors();
+        int numOfCores = Runtime.getRuntime().availableProcessors() - 1;
         ExecutorService executorService = Executors.newFixedThreadPool(numOfCores);
         MutationFramework mutationFramework = new MutationFramework();
         mutationFramework.setProjectPath(projectPath);
         mutationFramework.setMutator(new HeuristicMutator());
         List<TestCase> testCaseList = mutationFramework.getTestCases();
+        String createdBugsFilePath = String.join(File.separator, repositoryPath, projectName, CREATED_BUGGY_PROJECT_FILE);
+        JSONObject storedProjects = getStoredProjects(createdBugsFilePath);
         for (TestCase testCase : testCaseList) {
             mutationFramework.setTestCase(testCase);
             List<MutationCommand> commands;
@@ -50,8 +58,37 @@ public class DatasetCreator {
                 continue;
             }
             for (MutationCommand command : commands) {
-                executorService.submit(new BuggyProjectCreator(repositoryPath, projectPath, projectName, testCase, command));
+                BuggyProject buggyProject = new BuggyProject(testCase, command, projectName);
+                if (checkBuggyProjectAlreadyCloned(storedProjects, buggyProject)) {
+                    continue;
+                }
+                executorService.submit(new BuggyProjectCreator(repositoryPath, projectPath, buggyProject,
+                        createdBugsFilePath));
             }
+        }
+    }
+
+    public static JSONObject getStoredProjects(String pathToFile) {
+        /**
+         * {
+         * "some hash or identifier, up to buggy project obj" : {
+         *     the fields in string version
+         * }
+         * }
+         */
+        try {
+            return JSONWrapper.getJSONObjectFromFile(pathToFile);
+        } catch (RuntimeException e) {
+            return new JSONObject();
+        }
+    }
+
+    private static boolean checkBuggyProjectAlreadyCloned(JSONObject createdProjects, BuggyProject project) {
+        try {
+            createdProjects.get(project.key());
+            return true;
+        } catch (JSONException e) {
+            return false;
         }
     }
 }
